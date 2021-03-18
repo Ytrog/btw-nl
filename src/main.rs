fc242aa3ada0098fc861290bcf8ae858c817868c
Ytrog <hisbrucker@gmail.com>
2021-03-18 16:20:22 +0100

    Bruto laag and cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use dialoguer::{Confirm, Select, console::Term, console::Style, Input};
-use std::convert;
+use dialoguer::{console::Style, console::Term, Confirm, Input, Select};
 
 enum Action {
     BtwBrutoHoog,
     BtwBrutoLaag,
     BtwNettoHoog,
     BtwNettoLaag,
-    Stop
+    Stop,
 }
 
 /// pause console
@@ -15,30 +14,42 @@ fn pause() {
     Term::stdout().read_key().unwrap();
 }
 
+/// clear the screen
+fn clear() {
+    Term::stdout().clear_screen().unwrap();
+}
+
 fn ask_bruto(percentage: u8) {
     let input: f64 = Input::new().with_prompt("Bedrag").interact_text().unwrap();
     println!("{}", calc_bruto(input, percentage));
     pause();
 }
 
-fn calc_bruto(value: f64, percentage: u8) -> f64{
+fn calc_bruto(value: f64, percentage: u8) -> f64 {
     let p = f64::from(percentage); // u8 to prevent negatives
-    value/(100.0 + p) * p
+    value / (100.0 + p) * p
 }
 
-fn stop() -> bool{
-    Confirm::new().with_prompt("Stoppen?").interact().unwrap_or(true)
+fn stop() -> bool {
+    Confirm::new()
+        .with_prompt("Stoppen?")
+        .interact()
+        .unwrap_or(true)
 }
 
 fn get_action() -> Action {
+    clear();
     let action = Select::new()
-    .with_prompt("Wat wilt u doen (gebruik pijltjestoetsen om te selecteren en vervolgens enter)")
-    .item("Btw hoog berekenen vanuit bruto bedrag")
-    .item("Btw laag berekenen vanuit bruto bedrag")
-    .item("Btw hoog berekenen vanuit netto bedrag")
-    .item("Btw laag berekenen vanuit netto bedrag")
-    .item(Style::new().red().bright().apply_to("Stoppen"))
-    .interact().unwrap();
+        .with_prompt(
+            "Wat wilt u doen (gebruik pijltjestoetsen om te selecteren en vervolgens enter)",
+        )
+        .item("Btw hoog berekenen vanuit bruto bedrag")
+        .item("Btw laag berekenen vanuit bruto bedrag")
+        .item("Btw hoog berekenen vanuit netto bedrag")
+        .item("Btw laag berekenen vanuit netto bedrag")
+        .item(Style::new().red().bright().apply_to("Stoppen"))
+        .interact()
+        .unwrap();
 
     match action {
         0 => Action::BtwBrutoHoog,
@@ -46,7 +57,7 @@ fn get_action() -> Action {
         2 => Action::BtwNettoHoog,
         3 => Action::BtwNettoLaag,
         4 => Action::Stop,
-        _ => Action::Stop
+        _ => Action::Stop,
     }
 }
 
@@ -57,12 +68,15 @@ fn main() {
     term.set_title("BTW berekenaar");
 
     loop {
-    match get_action() {
-        Action::BtwBrutoHoog => ask_bruto(21),
-        Action::Stop => if stop() {
-            break;
-        },
-        _ => unreachable!()
-    }
+        match get_action() {
+            Action::BtwBrutoHoog => ask_bruto(21),
+            Action::BtwBrutoLaag => ask_bruto(9),
+            Action::Stop => {
+                if stop() {
+                    break;
+                }
+            }
+            _ => unreachable!(),
+        }
     }
 }
