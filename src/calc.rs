5e3a000ca1d437d28fe6b17463d9cb71e6334c28
Ytrog <hisbrucker@gmail.com>
2021-03-19 10:23:13 +0100

    Added a test

--- src/calc.rs
+++ src/calc.rs
@@ -1,10 +1,12 @@
 use std::fmt;
+//use std::cmp;
 
+#[derive(Debug, PartialEq)]
 pub struct Amount {
     netto: f64,
     bruto: f64,
     tax: f64,
-    percentage: u8
+    percentage: u8,
 }
 
 impl std::fmt::Display for Amount {
@@ -12,8 +14,11 @@ impl std::fmt::Display for Amount {
         // The `f` value implements the `Write` trait, which is what the
         // write! macro is expecting. Note that this formatting ignores the
         // various flags provided to format strings.
-        write!(f, "Netto: {:.2}\nBruto: {:.2}\nBtw bedrag: {:.2}\nBtw percentage: {}"
-        , self.netto, self.bruto, self.tax, self.percentage)
+        write!(
+            f,
+            "Netto: {:.2}\nBruto: {:.2}\nBtw bedrag: {:.2}\nBtw percentage: {}",
+            self.netto, self.bruto, self.tax, self.percentage
+        )
     }
 }
 
@@ -21,11 +26,11 @@ impl std::fmt::Display for Amount {
 pub fn calc_bruto(value: f64, percentage: u8) -> Amount {
     let p = f64::from(percentage); // u8 to prevent negatives
     let tax = value / (100.0 + p) * p;
-    Amount{
-        tax: tax, 
+    Amount {
+        tax,
         bruto: value,
         netto: value - tax,
-        percentage: percentage
+        percentage,
     }
 }
 
@@ -33,10 +38,30 @@ pub fn calc_bruto(value: f64, percentage: u8) -> Amount {
 pub fn calc_netto(value: f64, percentage: u8) -> Amount {
     let p = f64::from(percentage); // u8 to prevent negatives
     let tax = value * p / 100.0;
-    Amount{
-        tax: tax, 
+    Amount {
+        tax,
         bruto: value + tax,
         netto: value,
-        percentage: percentage
+        percentage,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::calc::*;
+
+    #[test]
+    fn bruto_hoog_correct() {
+        let bruto = 121.0;
+        let expected = Amount {
+            tax: 21.0,
+            bruto: bruto,
+            netto: 100.0,
+            percentage: 21,
+        };
+
+        let actual = calc_bruto(bruto, 21);
+
+        assert_eq!(expected, actual);
     }
 }
