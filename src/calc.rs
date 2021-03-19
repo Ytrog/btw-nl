e1a0cc995f0b9965f1961b9334ad3206ffe0d701
Ytrog <hisbrucker@gmail.com>
2021-03-19 10:02:57 +0100

    Implement Display for Amount

--- src/calc.rs
+++ src/calc.rs
@@ -1,11 +1,42 @@
-/// calc from bruto
-pub fn calc_bruto(value: f64, percentage: u8) -> f64 {
-    let p = f64::from(percentage); // u8 to prevent negatives
-    value / (100.0 + p) * p
+use std::fmt;
+
+pub struct Amount {
+    netto: f64,
+    bruto: f64,
+    tax: f64,
+    percentage: u8
 }
 
-pub fn calc_netto(value: f64, percentage: u8) -> f64 {
+impl std::fmt::Display for Amount {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // The `f` value implements the `Write` trait, which is what the
+        // write! macro is expecting. Note that this formatting ignores the
+        // various flags provided to format strings.
+        write!(f, "Netto: {}\nBruto: {}\nBtw bedrag: {}\nBtw percentage: {}"
+        , self.netto, self.bruto, self.tax, self.percentage)
+    }
+}
+
+/// calc from bruto
+pub fn calc_bruto(value: f64, percentage: u8) -> Amount {
     let p = f64::from(percentage); // u8 to prevent negatives
-    dbg!(p);
-    value * p / 100.0
+    let tax = value / (100.0 + p) * p;
+    Amount{
+        tax: tax, 
+        bruto: value,
+        netto: value - tax,
+        percentage: percentage
+    }
+}
+
+/// calc from netto
+pub fn calc_netto(value: f64, percentage: u8) -> Amount {
+    let p = f64::from(percentage); // u8 to prevent negatives
+    let tax = value * p / 100.0;
+    Amount{
+        tax: tax, 
+        bruto: value + tax,
+        netto: value,
+        percentage: percentage
+    }
 }
