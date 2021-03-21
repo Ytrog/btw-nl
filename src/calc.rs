37ffa83d4cb6a42573e3ef5ac9d7ab0a212a08f2
Ytrog <hisbrucker@gmail.com>
2021-03-21 11:24:45 +0100

    Changed test

--- src/calc.rs
+++ src/calc.rs
@@ -88,7 +88,7 @@ mod tests {
         let actual_bruto = calc_bruto(bruto, 21);
         let actual_netto = calc_netto(netto, 21);
 
-        assert_eq!(actual_bruto.bruto, actual_netto.bruto);
+        assert_eq!(format!("{}", actual_bruto), format!("{}", actual_netto));
     }
 
     #[test]
